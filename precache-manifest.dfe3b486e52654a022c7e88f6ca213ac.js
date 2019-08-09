5d4dcb0f45b371ce96957cc525a0ce9a5400d042
jsevern94 <severnj@spu.edu>
2019-08-08 17:59:50 -0700

    Updates

---
+++ precache-manifest.dfe3b486e52654a022c7e88f6ca213ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee12cc87f0d81dbcf0a9d5ed02f4e20",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9e6adc4a2de872803488",
+    "url": "/clicky-game/static/css/main.26c9c9c1.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clicky-game/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "9e6adc4a2de872803488",
+    "url": "/clicky-game/static/js/main.15b1231c.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "86a7fe42ca01325c9bef6831c5abef0b",
+    "url": "/clicky-game/static/media/banner.86a7fe42.jpg"
+  }
+]);
